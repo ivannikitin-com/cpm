7eac14a09031cf70ae4b8a7681afb416ae3b1ae9
Ivan Nikitin <ivan.g.nikitin@gmail.com>
2021-09-01 18:29:07 +0300

    Коррекция переводов и вывода задач

--- includes/pro/views/task/my-task.php
+++ includes/pro/views/task/my-task.php
@@ -19,10 +19,11 @@ if ( isset( $_GET['user_id'] ) && !empty( $_GET['user_id'] ) ) {
     $title           = __( 'My Tasks', 'cpm' );
 }
 
+// Текущий таб
 if ( isset( $_GET['tab'] ) ) {
     $ctab = sanitize_text_field( $_GET['tab'] );
 } else {
-    $ctab = 'overview';
+    $ctab = 'current';  // Таб по умолчанию
 }
 
 $user_id = apply_filters( 'cpm_my_task_user_id', $user_id );
@@ -61,21 +62,31 @@ $ctab  = apply_filters( 'cpm_my_task_tab', $ctab );
             <ul class="cpm-col-10 cpm-my-task-menu">
 
                 <li  class="<?php if ( $ctab == 'overview' ) echo 'active' ?>">
-                    <a href="<?php echo cpm_url_my_task() ?>" class="cpm-my-taskoverview" data-item="overview" data-user="<?php echo $user_id ?>"><?php _e( 'Overview', 'cpm' ); ?><div></div></a>
+                    <a href="<?php echo cpm_url_my_task() ?>" class="cpm-my-taskoverview" data-item="overview" data-user="<?php echo $user_id ?>">
+                        <?php _e( 'Task Overview', 'cpm' ); ?><div></div>
+                    </a>
                 </li>
                 <li class="<?php if ( $ctab == 'useractivity' ) echo 'active' ?>">
-                    <a href="<?php echo cpm_url_user_activity(); ?>" class="cpm-my-taskactivity" data-item="activity" data-user="<?php echo $user_id ?>"><?php _e( 'Activity', 'cpm' ); ?><div></div></a>
+                    <a href="<?php echo cpm_url_user_activity(); ?>" class="cpm-my-taskactivity" data-item="activity" data-user="<?php echo $user_id ?>">
+                        <?php _e( 'Task Activity', 'cpm' ); ?><div></div>
+                    </a>
                 </li>
 
                 <li class="<?php if ( $ctab == 'current' ) echo 'active' ?>">
-                    <a href="<?php echo cpm_url_current_task(); ?>" data-item="current" data-user="<?php echo $user_id ?>" class="cpm-my-currenttask"><?php _e( 'Current Task', 'cpm' ); ?> <div ><?php echo $count[ __( 'Current', 'cpm' ) ]; ?></div></a>
+                    <a href="<?php echo cpm_url_current_task(); ?>" data-item="current" data-user="<?php echo $user_id ?>" class="cpm-my-currenttask">
+                        <?php _e( 'Current Tasks', 'cpm' ); ?> <div ><?php echo $count[ __( 'Current', 'cpm' ) ]; ?></div>
+                    </a>
                 </li>
 
                 <li class="<?php if ( $ctab == 'outstanding' ) echo 'active' ?>">
-                    <a href="<?php echo cpm_url_outstanding_task(); ?>" data-item="outstanding" data-user="<?php echo $user_id ?>" class="cpm-my-outstandigntask"><?php _e( 'Outstanding Task', 'cpm' ); ?> <div><?php echo $count[ __( 'Outstanding', 'cpm' ) ]; ?></div></a>
+                    <a href="<?php echo cpm_url_outstanding_task(); ?>" data-item="outstanding" data-user="<?php echo $user_id ?>" class="cpm-my-outstandigntask">
+                        <?php _e( 'Outstanding Tasks', 'cpm' ); ?> <div><?php echo $count[ __( 'Outstanding', 'cpm' ) ]; ?></div>
+                    </a>
                 </li>
                 <li class="<?php if ( $ctab == 'complete' ) echo 'active' ?>">
-                    <a href="<?php echo cpm_url_complete_task(); ?>" data-item="complete" data-user="<?php echo $user_id ?>"  class="cpm-my-completetask"><?php _e( 'Completed Task', 'cpm' ); ?> <div ><?php echo $count[ __( 'Completed', 'cpm' ) ]; ?></div></a>
+                    <a href="<?php echo cpm_url_complete_task(); ?>" data-item="complete" data-user="<?php echo $user_id ?>"  class="cpm-my-completetask">
+                        <?php _e( 'Completed Tasks', 'cpm' ); ?> <div ><?php echo $count[ __( 'Completed', 'cpm' ) ]; ?></div>
+                    </a>
                 </li>
             </ul>
             <div class="cpm-col-2 cpm-sm-col-12 cpm-user-select">
